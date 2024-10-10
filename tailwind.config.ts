125b896bb48937b21df4b6b08e86fc1e0708088d
Tymur Biedukhin <tymurbiedukhin@Tymurs-MBP.lan>
2024-10-10 13:51:15 +0100

    chore: init commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,10 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
-export default config;
+}
+export default config
