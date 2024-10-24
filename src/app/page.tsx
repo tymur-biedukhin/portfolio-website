7513338f2edc4cceff8756576c25d449e3aaec87
Tymur Biedukhin <tymurbiedukhin@Tymurs-MacBook-Pro.local>
2024-10-24 18:14:31 +0100

    feat; favicon, root layout, project skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <span className="text-red-500">Hello world!</span>
+  return (
+    <h1 className="absolute bottom-12 left-[calc(20%+2.5rem)] text-xl text-white font-light max-w-[560px]">
+      Hello, I’m{' '}
+      <span className="text-[#4bffa5] font-bold">Tymur Biedukhin</span>, Team
+      Lead and Senior Software Engineer Based in Lisbon, Portugal.
+    </h1>
+  )
 }
---
+++ src/shared/navigation.ts
@@ -0,0 +1,5 @@
+export const Routes = {
+  HOME: '/',
+  WORKS: '/works',
+  CONTACT: '/contact',
+}
